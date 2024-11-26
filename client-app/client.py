bbfc8906831f0a0e5cc3233261d4b78906ad2882
Joshua Eastman <joshuabeastman@gmail.com>
2024-11-26 14:57:49 -0600

    updates README

--- client-app/client.py
+++ client-app/client.py
@@ -13,5 +13,15 @@ def send_message_to_bot(message):
     except Exception as e:
         print(f"Error sending message: {str(e)}")
 
+def something_complex():
+    i = 0
+    while i < 10:
+        i = i + 1
+        send_message_to_bot(f"Added 1 to {i -1}. Your new number is {i}")
+
 # Send string to discord bot listener
 send_message_to_bot("Hello, this is a message from Docker!")
+# something_complex()
+
+# add message to queue with timestamp (wrapper)
+# FIFO
